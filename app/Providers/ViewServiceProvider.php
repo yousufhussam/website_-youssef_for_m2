d96ee18bda561775ed651f93f7756cd09c5383af
Exynox <exynox@old-metin2.com>
2024-12-31 20:40:58 +0200

    Reordered view composers

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -23,8 +23,11 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Main application
         Facades\View::composer('layouts.app', ThemeComposer::class);
-        Facades\View::composer('layouts.mall', MallComposer::class);
         Facades\View::composer('layouts.app', HighscoreComposer::class);
+
+        // Mall
+        Facades\View::composer('layouts.mall', MallComposer::class);
     }
 }
